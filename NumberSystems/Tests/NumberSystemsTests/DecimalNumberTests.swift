feb77e57b2d77b8dff20eee2d7779dd31ebb792a
Jacob Gordon <bjacobgordon@gmail.com>
2024-11-29 15:49:31 -0600

    refactor: converts DecimalNumber_Base to DecimalNumber.Base

--- NumberSystems/Tests/NumberSystemsTests/DecimalNumberTests.swift
+++ NumberSystems/Tests/NumberSystemsTests/DecimalNumberTests.swift
@@ -2,5 +2,5 @@ import Testing
 @testable import NumberSystems
 
 @Test func testBaseDecimalNumbers() async throws {
-    #expect(DecimalNumber_Base.allCases.allSatisfy { $0.quantityMatchesCardinality })
+    #expect(DecimalNumber.Base.allCases.allSatisfy { $0.quantityMatchesCardinality })
 }
