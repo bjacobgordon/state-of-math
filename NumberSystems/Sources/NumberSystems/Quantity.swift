43f1e3bf10a80c1758ee2826ee76c3f9ee4deb6d
Jacob Gordon <bjacobgordon@gmail.com>
2024-12-04 20:55:16 -0600

    feature: adds support for Quantity predecession

--- NumberSystems/Sources/NumberSystems/Quantity.swift
+++ NumberSystems/Sources/NumberSystems/Quantity.swift
@@ -39,6 +39,12 @@ extension Quantity: Operable {
         let newElement = self.embodiment.first ?? Quantity.standardEmbodyingElement
         self.embodiment.append(newElement)
     }
+    
+    public mutating func precede() {
+        guard (self != Quantity.none) else { fatalError("There is no precedent for a lack of quantity") }
+        
+        self.embodiment.removeLast()
+    }
 }
 
 extension String {
