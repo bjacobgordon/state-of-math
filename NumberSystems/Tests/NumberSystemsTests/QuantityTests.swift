ea41edffe5b1790ff3ca3149cfb51e6885b73791
Jacob Gordon <bjacobgordon@gmail.com>
2024-12-09 20:37:54 -0600

    feature: adds support for Quantity succession

--- NumberSystems/Tests/NumberSystemsTests/QuantityTests.swift
+++ NumberSystems/Tests/NumberSystemsTests/QuantityTests.swift
@@ -47,3 +47,16 @@ func decimalIntegerToQuantity(_ givenCount: Int) async throws {
     let equivalentQuantity = givenCount.asQuantity
     #expect(givenCount.represents(equivalentQuantity))
 }
+
+@Test("Successor of quantities", arguments: [
+    0,
+    10,
+    100,
+    1000
+])
+func successorOfQuantity(_ givenCount: Int) async throws {
+    let operativeQuantity = givenCount.asQuantity
+    let succeededCount    = givenCount + 1
+    let succeededQuantity = operativeQuantity.successor
+    #expect(succeededCount.represents(succeededQuantity))
+}
